27c55d49fa689678a47bf2091127849fc09fe5ca
kira-hart <khart@optics.arizona.edu>
2021-07-28 10:47:28 -0700

    updating with version that is confirmed functional on the SBC

--- ImageCapture/image_capture.py
+++ ImageCapture/image_capture.py
@@ -32,8 +32,8 @@ def take_image(filename):
     now = datetime.datetime.now()
     OS_time = now.strftime("%H:%M")
 
-    camera1 = Boson(port='COM5')
-    camera2 = Boson(port='COM6')
+    camera1 = Boson(port='/dev/ttyACM0')
+    camera2 = Boson(port='/dev/ttyACM1')
 
     #set FFC to manual
     camera1.set_ffc_manual()
@@ -42,13 +42,12 @@ def take_image(filename):
     #get FPA temperature
     temp1 = camera1.get_fpa_temperature()
     temp2 = camera2.get_fpa_temperature()
+    print(temp1)
+    print(temp2)
 
     #Take Image
-    image1 = camera1.grab(device_id = 1)
-    image2 = camera2.grab(device_id = 2)
-    
-    print(image1)
-    print(temp1)
+    image1 = camera1.grab(device_id = 0)
+    image2 = camera2.grab(device_id = 1)
 
 
     #Close Camera
@@ -85,12 +84,9 @@ def main():
     #Check Current Working Directory (cwd)
     cwd = os.getcwd()
     directory = os.listdir(cwd)
-    print()
-    print("Directory Path %r: \n" % (cwd))
-    print("List of File Names: %s \n" % (directory))
 
-    #Check Destination Directory (cwd)
-    path = '/mnt/sdcard/image_data'
+    #Check Destination Directory (path)
+    path = '/mnt/sdcard/image_data/'
     directory = os.listdir(path)
     
     print()
@@ -101,17 +97,18 @@ def main():
     #File Name - Control Variable
     count = 0
     #Set to work on .hdf5 file type extension.
-    filename = "Capture" + str(count) + ".hdf5"
+    name = "Capture" + str(count) + ".hdf5"
     
     #Increments File Name, If File Already Exist!
-    while filename in directory:
+    while name in directory:
         count+=1
-        filename = "Capture" + str(count) + ".hdf5"
+        name = "Capture" + str(count) + ".hdf5"
     else:
         #Creates New File with next available value!
-        while filename not in directory:
-            filename = "Capture" + str(count) + ".hdf5"
-            print(filename)
+        while name not in directory:
+            name = "Capture" + str(count) + ".hdf5"
+            filename = os.path.join(path,name)
+            take_image(filename)
             count += 1
 
 

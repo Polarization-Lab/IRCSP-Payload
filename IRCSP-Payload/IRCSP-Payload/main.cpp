9f3be04cc52ea24d85bc2d82e9c37c2008aff36c
kira-hart <khart@optics.arizona.edu>
2021-08-04 14:48:32 -0700

    adding Adafruit package

---
+++ IRCSP-Payload/IRCSP-Payload/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  IRCSP-Payload
+//
+//  Created by Kira Hart on 8/4/21.
+//
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+//#include <linux/pci.h>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/mman.h>
+
+
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

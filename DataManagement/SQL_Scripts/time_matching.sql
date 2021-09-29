b664b558d58cbaeff71e49191ed89f04c9d4dba8
kira-hart <khart@optics.arizona.edu>
2021-09-28 17:26:34 -0700

    cleaning up bloated ipython notebooks

--- DataManagement/SQL_Scripts/time_matching.sql
+++ DataManagement/SQL_Scripts/time_matching.sql
@@ -1,4 +1,4 @@
-CREATE TABLE joint_telemetry select m.meas_id, m.time, m.cam1_temp,m.cam2_temp, t.hous_temp, t.humidity, t.pressure
+select m.meas_id, m.time, m.cam1_temp,m.cam2_temp, t.hous_temp, t.humidity, t.pressure
 from measurements m
 left join telemetry t on t.time= (
   select max(t2i.time)

289b22556d68f8ede5e1cc52946441c8f0becd5d
Jaclyn John <84813157+jaclynamberjohn@users.noreply.github.com>
2022-05-25 10:12:17 -0700

    Update P3_main.py

--- P3_flight/P3_main.py
+++ P3_flight/P3_main.py
@@ -1,36 +1,64 @@
+# -*- coding: utf-8 -*-
 """
 Created on Fri May 20 12:25:25 2022
 
 @author: jaclynjohn
 """
+from   flirpy.camera.boson import Boson 
+import matplotlib.pyplot as plt
+import h5py
+import numpy as np
+import time 
+import serial
+import csv
+
+
+def get_timestamp():
+    t = time.localtime()
+    return time.strftime('%H.%M.%S', t)
+
 
 def main():
     
-    from instrument_control import P3_image_capture
-    import time 
+    from p3_readsensors import p3_readsensors
+    from P3_image_capture import P3_image_capture
     
-    wait = 30 #seconds
+    
+    save_path = 'C:\\Users\\khart\\Documents\\Summer2022Campaign\\IRCSP1\\TestRuns\\streamingdata\\'
+
+    wait = 5 #seconds
     i = 0;
-    while i < 480: #over 4 hours later: each loop will be a little over than 30 seconds 
-    #since it will wait 30 seconds before doing everything again
+    while i < 40: 
+
+        timestamp = get_timestamp() #take time at start of measurement 
+        epoch = time.time()
+        
+        im1 = P3_image_capture()[0]
+        im2 = P3_image_capture()[1]
+        t1 = P3_image_capture()[2]
+        t2 = P3_image_capture()[3]
+
+        
+        with h5py.File(save_path +  'meas_' + str(i) + '.h5', 'w') as h5:
+            h5.attrs["timestamp"] = timestamp
+            h5.attrs['epochtime'] = epoch
+            h5["image1"] = im1
+            h5["image2"] = im2
+            h5["temp1"] = t1
+            h5["temp2"] = t2
+        
     
-        P3_image_capture()
-        #saves 10 frames, two temperatures, and a timestamp into h5 file on Hamilton laptop
-        #displays temperature of FPA
-        #displays image for each camera that is average of 10 frames to show SaSa students
-        
-        #P3_read_BME()
-        #will save pres,temp,hum from each sensor into one h5 file on Hamilton laptop
-        #will label it BME_1..2...3 etc to match meas_1...2...3 from image capture
-        #will print out info so I can monitor
-        
-        #P3_read_therm()
-        #will save temperature from each thermistor into one h5 file
-        #will label it therm_1..2...3 etc to match meas_1...2...3 from image capture
-        #will print out info so I can monitor
+        print('h5 file meas_' + str(i) + ' created')
 
+    
+        sensordata = p3_readsensors()
+        sensordata.append(timestamp)
         
-
+        with open(save_path + 'telemetry.csv', 'a') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(sensordata)
+        
         time.sleep(wait)
         i +=1
         
+

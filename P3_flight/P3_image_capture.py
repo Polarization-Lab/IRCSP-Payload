00a72e25faa4fc41d2e671475dc655699c0ffb6b
Jaclyn John <84813157+jaclynamberjohn@users.noreply.github.com>
2022-05-25 10:11:24 -0700

    Update P3_image_capture.py

--- P3_flight/P3_image_capture.py
+++ P3_flight/P3_image_capture.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 """
 Created on Wed May 11 15:46:32 2022
 
-@author: jaclyn john
+@author: jaclynjohn
 """
 
 
@@ -10,17 +11,12 @@ import matplotlib.pyplot as plt
 import h5py
 import numpy as np
 import time 
+
 #from datetime import datetime
 
-def get_timestamp():
-    t = time.localtime()
-    return time.strftime('%H_%M_%S', t)
 
-
-def p3_image_capture():
+def P3_image_capture():
     
-    save_path = 'C:\\Users\\khart\\Documents\\Summer2022Campaign\\IRCSP1\\TestRuns\\StreamingDataTest\\'
-    meas_num = 1
     frame_avg = 10
 
     """DO NOT CHANGE"""
@@ -31,47 +27,38 @@ def p3_image_capture():
     camera1.set_ffc_manual() 
     camera2.set_ffc_manual()
 
+    img1 = np.zeros((256,320))
+    img2 = np.zeros((256,320))
+    im1 = np.zeros((frame_avg,256,320))
+    im2 = np.zeros((frame_avg,256,320))
+    
+    t1 = camera1.get_fpa_temperature()
+    t2 = camera2.get_fpa_temperature()
+    
+    print('cam 1 at ' + str(t1))
+    print('cam 2 at ' + str(t2))
 
-    for i in range(meas_num):
+    for j in range(frame_avg):
+
+            #take image
+        im1[j,:,:] = camera1.grab(device_id = 1)
+        im2[j,:,:] = camera2.grab(device_id = 2)
+            
+    img1[:,:] = np.mean(im1,axis = 0 )
+    img2[:,:] = np.mean(im2,axis = 0 )
+
+    fig, ax = plt.subplots(2,1)
+    ax[0].imshow(img1[:,:])
+    ax[1].imshow(img2[:,:])
+    plt.show()
     
-        img1 = np.zeros((256,320))
-        img2 = np.zeros((256,320))
-        im1 = np.zeros((frame_avg,256,320))
-        im2 = np.zeros((frame_avg,256,320))
-        
-        t1 = camera1.get_fpa_temperature()
-        t2 = camera2.get_fpa_temperature()
-        
-        print('cam 1 at ' + t1)
-        print('cam 2 at ' + t2)
     
-        for j in range(frame_avg):
-    
-                #take image
-            im1[j,:,:] = camera1.grab(device_id = 1)
-            im2[j,:,:] = camera2.grab(device_id = 2)
-                
-        img1[:,:] = np.mean(im1,axis = 0 )
-        img2[:,:] = np.mean(im2,axis = 0 )
-    
-        fig, ax = plt.subplots(2,1)
-        ax[0].imshow(img1[:,:])
-        ax[1].imshow(img2[:,:])
-        plt.show()
-        
-        time_stamp = get_timestamp()
-        with h5py.File(save_path + 'meas_'  +  str(i+1) + '.h5', 'w') as h5:
-            h5.attrs["time_stamp"] = time_stamp
-            h5["image1"] = im1
-            h5["image2"] = im2
-            h5["temp1"] = t1
-            h5["temp2"] = t2
-    
-        print('h5 file meas_ ' + str(i) + 'created')
-        
-        
-       
+    return im1,im2,t1,t2
+           
     camera1.close()
     camera2.close()
+    
+    
 
+        
         

ee627321a402632d6f47c363fe493b852405a8d4
Jaclyn John <84813157+jaclynamberjohn@users.noreply.github.com>
2022-08-10 17:12:18 -0700

    Create TEC_set_flight_temp.py

---
+++ Balloon_Flight_2022/TEC_set_flight_temp.py
@@ -0,0 +1,11 @@
+from mecom import MeCom, ResponseException, WrongChecksum
+from example import MeerstetterTEC
+
+self = MeCom;
+MeCom.channel = 1;
+MeCom.session = MeCom;
+
+MeerstetterTEC.set_temp(self,-20.0)
+    
+
+

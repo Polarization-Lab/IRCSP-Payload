8035005331caca9d08872f2bf2e1ae21a7fca427
Jaclyn John <84813157+jaclynamberjohn@users.noreply.github.com>
2023-12-19 12:11:58 -0700

    Create arduino.ino

---
+++ Arduino2023/arduino.ino
@@ -0,0 +1,109 @@
+
+/***************************************************************************
+reads 3 BME sensors and a thermister
+thermister read found here: https://pythonforundergradengineers.com/python-arduino-potentiometer.html
+BME read found here: http://cactus.io/hookups/sensors/barometric/bme280/hookup-arduino-to-multiple-bme280-barometric-pressure-sensors
+ ***************************************************************************/
+
+#include <Wire.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+
+#define TCAADDR 0x70
+
+/* Initialize BMEs */
+Adafruit_BME280 bme1;
+Adafruit_BME280 bme2;
+Adafruit_BME280 bme3;
+/*Adafruit_BME280 bme3;
+
+/* Initialise thermister */
+int sensorPin = A3;
+int contextPin = A0; //A1
+int lensPin = A2;
+int heatPin = A1; //A0
+int sensorVal;
+int contextVal;
+int lensVal;
+int heatVal;
+
+/* for reading altitude
+#define SEALEVELPRESSURE_HPA (1013.25)
+Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+*/
+
+unsigned long delayTime;
+
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+ 
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
+
+void setup()
+{
+    while (!Serial);
+    delay(1000);
+
+    Wire.begin();
+    
+    Serial.begin(9600);
+
+    /* Initialise sensors */
+      tcaselect(1);
+      if(!bme1.begin())
+    {
+      //Serial.println("No BME280 detected at Multiplexer Port 1!");
+      //while(1);
+    }
+    
+    tcaselect(2);
+    if(!bme2.begin())
+    {
+      //Serial.println("No BME280 detected at Multiplexer Port 2!");
+      //while(1);
+    }
+
+    tcaselect(6);
+    if(!bme3.begin())
+    {
+      //Serial.println("No BME280 detected at Multiplexer Port 6!");
+     //while(1);
+   }
+
+}
+    
+void loop() 
+{
+  
+    //read BMEs
+    tcaselect(1);
+   // Serial.print("bme 1 ");
+    Serial.print(bme1.readPressure()); Serial.print(",");
+    Serial.print(bme1.readHumidity()); Serial.print(",");  //%
+    Serial.print(bme1.readTemperature()); Serial.print(",");   //°C
+    tcaselect(2);
+   // Serial.print("bme 2 ");
+    Serial.print(bme2.readPressure()); Serial.print(",");
+    Serial.print(bme2.readHumidity()); Serial.print(",");
+    Serial.print(bme2.readTemperature()); Serial.print(",");
+    tcaselect(6);
+   // Serial.print("bme 3 ");
+    Serial.print(bme3.readPressure()); Serial.print(",");
+    Serial.print(bme3.readHumidity()); Serial.print(",");
+    Serial.print(bme3.readTemperature()); Serial.print(",");
+
+    //read thermister
+    sensorVal = analogRead(sensorPin);
+    contextVal = analogRead(contextPin);
+    lensVal = analogRead(lensPin);
+    heatVal = analogRead(heatPin);
+    //float thermV= sensorVal * (5.0 / 1023.0);   //converts analog read value to voltage
+    Serial.print(contextVal); Serial.print(',');
+    Serial.print(lensVal); Serial.print(',');
+    Serial.print(sensorVal); Serial.print(',');
+    Serial.print(heatVal); 
+    Serial.print('\n');
+}
